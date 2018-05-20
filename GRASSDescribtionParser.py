705523b827b5efb7a85d8e234b8794a83e8159c5
Radek Novotný <radeknovotny94@gmail.com>
2018-05-21 00:40:14 +0200

    Add first QGIS parameters to parser

--- GRASSDescribtionParser.py
+++ GRASSDescribtionParser.py
@@ -1,6 +1,22 @@
 import os
 import xml.etree.ElementTree
 
+def print_name_desc(modul):
+    print(modul.attrib['name'].strip() + '|' + \
+          modul.find("description").text.strip() + '|'),
+
+def print_default(parameter):
+    if parameter.tag == 'default':
+        print(parameter.find("default").text.strip() + '|'),
+    else:
+        print('None|'),
+
+def print_optional(parameter):
+    if parameter.attrib['required'] == 'yes':
+        print('False')
+    else:
+        print('True')
+
 file = open(os.path.join("C:\Users","radek","Desktop","r_covar.xml"), "r")
 # print file.read()
 
@@ -27,14 +43,48 @@ else:
 
 for child in root:
     if child.tag == 'parameter':
-        if child.attrib['type'] == 'string' and \
-           child.attrib['required'] == 'yes' and \
-           child.attrib['multiple'] == 'yes':
-            print 'QgsProcessingParameterMultipleLayers|'
-        print child.attrib['name'] + '|'
-        print child.find("description").text.strip()
+        if child.attrib['multiple'] == 'yes':
+            print('QgsProcessingParameterMultipleLayers|'),
+            print_name_desc(child)
+            for k in child:
+                if k.tag == 'gisprompt':
+                    if k.attrib['prompt'] == 'raster':
+                        print('TypeRaster|'),
+                        print_default(k)
+                        print_optional(child)
+                    if k.attrib['prompt'] == 'vector':
+                        print('TypeVector|'),
+                        print_default(k)
+                        print_optional(child)
+
+        if child.attrib['multiple'] == 'no':
+            for k in child:
+                if k.tag == 'gisprompt':
+                    if k.attrib['prompt'] == 'raster':
+                        print('QgsProcessingParameterRasterLayer|'),
+                    if k.attrib['prompt'] == 'vector':
+                        print('QgsProcessingParameterVectorLayer|'),
+                print_name_desc(child)
+                print_default(k)
+                print_optional(child)
+
+        if child.attrib['type'] == 'string':
+            for k in child:
+                if not k.tag.find('gisprompt'):
+                    print(k.tag)
+                    print('QgsProcessingParameterString|'),
+                    print_name_desc(child)
+                    print_default(k)
+                    print('True|')
 
 
+
+# for child in root:
+#     for k in child:
+#         if k.tag == 'gisprompt':
+#             print(k.tag)
+#             if k.attrib['prompt'] == 'raster':
+#                 print('raster')
 # print root.tag, root.attrib
 
 # for atype in root.findall('name'):
